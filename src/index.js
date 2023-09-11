533b0cfbb51ef394ba011b126096f40d89564eab
Adrien BROWN <Brown.Adrien@lsmbo.u-strasbg.fr>
2023-09-11 13:14:50 +0200

    main page update

--- src/index.js
+++ src/index.js
@@ -95,10 +95,6 @@ app.whenReady().then(() => {
     console.log('Received form data in ipcMain:', formDataObject);
     const terminalOutput = await executeScript(formDataObject);
 
-    // Rediriger vers la page d'infos une fois l'exécution terminée
-    const mainWindow = BrowserWindow.getFocusedWindow();
-    mainWindow.loadFile(path.join(__dirname, 'infos.html'));
-
     return terminalOutput;
   });
 
--- src/renderer.js
+++ src/renderer.js
@@ -1,6 +1,8 @@
 // Récupération des éléments du DOM
 const msForm = document.getElementById('msForm');
-const terminalDiv = document.getElementById('terminal');
+const loadedAnalysisDiv = document.getElementById('loaded-analysis');
+const dialogBox = document.querySelector('.dialog-box');
+const dialogBoxOkButton = document.querySelector('.dialog-box-content button');
 
 // Appelle la fonction toggleField() lors du chargement initial de la page
 document.addEventListener('DOMContentLoaded', toggleField);
@@ -9,7 +11,6 @@ document.addEventListener('DOMContentLoaded', toggleField);
 msForm.addEventListener('submit', async (event) => {
   // Empêche le formulaire de se rafraîchir lorsqu'il est soumis
   event.preventDefault();
-
   // Récupère les valeurs du formulaire
   const formData = new FormData(event.target);
   const formDataObject = Object.fromEntries(formData.entries());
@@ -28,20 +29,60 @@ msForm.addEventListener('submit', async (event) => {
   }
 
   try {
+    // Afficher un chargement pendant l'exécution
+    showLoading();
+
     // Appelle la fonction 'clickSubmit' du processus principal (index.js) en passant les données du formulaire
     const terminalOutput = await window.electronAPI.clickSubmit(formDataObject);
 
-    // Affiche la sortie du terminal dans la div de terminal
-    terminalDiv.innerText = terminalOutput;
+    // Cacher le chargement une fois que l'exécution est terminée
+    hideLoading();
+
+    // Afficher une boîte de dialogue de succès
+    showSuccessDialog();
+
+    // Créer un nouvel élément li avec le nom de l'analyse
+    const newListItem = document.createElement('li');
+    newListItem.textContent = formDataObject.name;
+
+    // Ajouter le nouvel élément à la liste
+    loadedAnalysisDiv.appendChild(newListItem);
 
     // Réinitialiser le formulaire
     msForm.reset();
-
+    
   } catch (error) {
+    hideLoading();
     // En cas d'erreur, affiche le message d'erreur dans la div de terminal
-    terminalDiv.innerText = `An error occurred: ${error.message}\n\n${JSON.stringify(error, null, 2)}`;
+    loadedAnalysisDiv.innerText = `An error occurred: ${error.message}\n\n${JSON.stringify(error, null, 2)}`;
   }
 });
+
+dialogBoxOkButton.addEventListener('click', () => {
+  dialogBox.style.display = 'none';
+  const mainDiv = document.getElementById('window');
+  // Masquer la div window grise
+  mainDiv.style.display = 'none';
+});
+
+// Fonction pour afficher l'indicateur de chargement
+function showLoading() {
+  const loadingIndicator = document.getElementById('window');
+  const loadingIndicatorSpan = document.querySelector('.window span');
+  loadingIndicator.style.display = 'block';
+  loadingIndicatorSpan.style.display = 'block';
+}
+
+// Fonction pour masquer l'indicateur de chargement
+function hideLoading() {
+  const loadingIndicator = document.querySelector('.window span');
+  loadingIndicator.style.display = 'none';
+}
+
+function showSuccessDialog() {
+  const dialogBox = document.querySelector('.dialog-box');
+  dialogBox.style.display = 'block';
+}
 
 // Fonction pour gérer la visibilité du champ conditionnel
 function toggleField() {
@@ -51,9 +92,9 @@ function toggleField() {
    
   if (select.value === 'Ionbot' || select.value === 'Maxquant') {
     file_result.style.display = 'none';
-    folder_result.style.display = 'block';
+    folder_result.style.display = 'flex';
   } else {
-    file_result.style.display = 'block';
+    file_result.style.display = 'flex';
     folder_result.style.display = 'none';
   }
 }
